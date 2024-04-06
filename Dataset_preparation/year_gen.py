364e71f62bf5aec40b4b9d77f97fa0d547c006f8
ChandanVK6 <chandanvk002@gmail.com>
2024-04-06 19:07:32 +0530

    images

--- Dataset_preparation/year_gen.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-df = pd.read_csv("Dataset_preparation/Database/data_csv_format.csv")
-financial_df = pd.read_csv("Dataset_preparation/Database/financial_data.csv")
-print(df['caledar_year'])
-for i, year in df['caledar_year'].items():
-    financial_df.at[i, '[year]'] = str(year)
-financial_df.to_csv("Dataset_preparation/Database/financial_data.csv", index=False)
